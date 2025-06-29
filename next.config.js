4f18d876fb1673b07656471ffbc4acc8ba5411e2
BenJr23 <rubenjrtbertuso@gmail.com>
2025-06-30 03:14:40 +0800

    one good run XD

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'zbgnnmibqcwkcmawktxt.supabase.co/',
+        hostname: 'snqwgmourfpyfsvxcjyd.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/lms/**',
       },
